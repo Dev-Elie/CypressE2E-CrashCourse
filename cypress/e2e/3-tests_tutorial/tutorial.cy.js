d86aaeaf19484913304d3d181051ad75c17ce61d
Dev-Elie <eochieng9448@gmail.com>
2022-08-14 15:28:18 +0300

    first commit

---
+++ cypress/e2e/3-tests_tutorial/tutorial.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('E2E Test Site', ()=>{
+    it("Is site reachable",()=>{
+        cy.visit("https://codedamn.com/")
+        cy.contains("Learn Programming Interactively").should("exist")
+    })
+
+    // Sign up page
+    it.only("Test sign up page",()=>{
+        cy.viewport(1280, 720)
+        cy.visit("https://codedamn.com/")
+        cy.contains("Sign in").click()
+        cy.contains("Sign in to codedamn").should("exist")
+        cy.contains("Email address / Username").should("exist")
+        cy.contains("Password").should("exist")
+        cy.contains("Don't have an account? Create one").should("exist")
+
+    })
+})
